4e36be3c77fbf8dd59494002332fa335dc922b44
Grayson Halliday <graysonhalliday@Graysons-MacBook-Pro-6.local>
2020-12-20 17:07:39 -0600

    First draft of personal website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Navbar, NavDropdown, Nav} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Education from './Components/Education.js'
+import WorkExperience from './Components/WorkExperience.js'
+import ProjectComponent from './Components/ProjectComponent.js'
+import HomeComponent from './Components/HomeComponent.js'
+import ContactComponent from './Components/ContactComponent.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar.Brand as={Link} to="/">Grayson Halliday</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link as={Link} to="/education">Education</Nav.Link>
+            <Nav.Link as={Link} to="/work-experience">Work Experience</Nav.Link>
+            <NavDropdown title="Personal Projects" id="basic-nav-dropdown">
+              <NavDropdown.Item as={Link} to="/team-solve">TeamSolve iOS Application</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/led-display">Network-Connected Audio-Sensing LED Display</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/facial-recognition">Facial Recognition Greeting System</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/fatigue-detection">Fatigue Detecting Neural Net</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/audio-effects">Bluetooth-Connected Real-Time Audio Effects Device</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Switch>
+          <Route exact path="/">
+            <HomeComponent></HomeComponent>
+          </Route>
+          <Route path="/education">
+            <Education></Education>
+          </Route>
+          <Route path="/work-experience">
+            <WorkExperience></WorkExperience>
+          </Route>
+          <Route path="/team-solve">
+            <ProjectComponent projectName="teamSolve"></ProjectComponent>
+          </Route>
+          <Route path="/led-display">
+            <ProjectComponent projectName="ledDisplay"></ProjectComponent>
+          </Route>
+          <Route path="/facial-recognition">
+            <ProjectComponent projectName="facialRecognition"></ProjectComponent>
+          </Route>
+          <Route path="/fatigue-detection">
+            <ProjectComponent projectName="fatigueDetection"></ProjectComponent>
+          </Route>
+          <Route path="/audio-effects">
+            <ProjectComponent projectName="audioEffects"></ProjectComponent>
+          </Route>
+          <Route path="/contact">
+            <ContactComponent></ContactComponent>
+          </Route>
+        </Switch>
+   </Router>
   );
 }
+
+
 
 export default App;
