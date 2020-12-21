43ba1c0f9e19da5581a2c3a0ee75dcd0b00e17b7
Grayson Halliday <graysonhalliday@Graysons-MacBook-Pro-6.local>
2020-12-21 13:45:23 -0600

    Block mobile devices

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import {BrowserView, MobileView} from 'react-device-detect';
 import WorkExperience from './Components/WorkExperience.js'
 import ProjectComponent from './Components/ProjectComponent.js'
 import HomeComponent from './Components/HomeComponent.js'
@@ -16,55 +17,64 @@ import EducationComponent from './Components/EducationComponent.js';
 
 function App() {
   return (
-   <Router>
-      <Navbar bg="dark" variant="dark" expand="lg">
-        <Navbar.Brand as={Link} to="/">Grayson Halliday</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link as={Link} to="/education">Education</Nav.Link>
-            <Nav.Link as={Link} to="/work-experience">Work Experience</Nav.Link>
-            <NavDropdown title="Personal Projects" id="basic-nav-dropdown">
-              <NavDropdown.Item as={Link} to="/team-solve">TeamSolve iOS Application</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/led-display">Network-Connected Audio-Sensing LED Display</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/facial-recognition">Facial Recognition Greeting System</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/fatigue-detection">Fatigue Detecting Neural Net</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/audio-effects">Bluetooth-Connected Real-Time Audio Effects Device</NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      <Switch>
-          <Route exact path="/">
-            <HomeComponent></HomeComponent>
-          </Route>
-          <Route path="/education">
-            <EducationComponent></EducationComponent>
-          </Route>
-          <Route path="/work-experience">
-            <WorkExperience></WorkExperience>
-          </Route>
-          <Route path="/team-solve">
-            <ProjectComponent projectName="teamSolve"></ProjectComponent>
-          </Route>
-          <Route path="/led-display">
-            <ProjectComponent projectName="ledDisplay"></ProjectComponent>
-          </Route>
-          <Route path="/facial-recognition">
-            <ProjectComponent projectName="facialRecognition"></ProjectComponent>
-          </Route>
-          <Route path="/fatigue-detection">
-            <ProjectComponent projectName="fatigueDetection"></ProjectComponent>
-          </Route>
-          <Route path="/audio-effects">
-            <ProjectComponent projectName="audioEffects"></ProjectComponent>
-          </Route>
-          <Route path="/contact">
-            <ContactComponent></ContactComponent>
-          </Route>
-        </Switch>
-   </Router>
+  <div>
+    <BrowserView>
+      <Router>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Navbar.Brand as={Link} to="/">Grayson Halliday</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link as={Link} to="/education">Education</Nav.Link>
+              <Nav.Link as={Link} to="/work-experience">Work Experience</Nav.Link>
+              <NavDropdown title="Personal Projects" id="basic-nav-dropdown">
+                <NavDropdown.Item as={Link} to="/team-solve">TeamSolve iOS Application</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/led-display">Network-Connected Audio-Sensing LED Display</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/facial-recognition">Facial Recognition Greeting System</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/fatigue-detection">Fatigue Detecting Neural Net</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/audio-effects">Bluetooth-Connected Real-Time Audio Effects Device</NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Switch>
+            <Route exact path="/">
+              <HomeComponent></HomeComponent>
+            </Route>
+            <Route path="/education">
+              <EducationComponent></EducationComponent>
+            </Route>
+            <Route path="/work-experience">
+              <WorkExperience></WorkExperience>
+            </Route>
+            <Route path="/team-solve">
+              <ProjectComponent projectName="teamSolve"></ProjectComponent>
+            </Route>
+            <Route path="/led-display">
+              <ProjectComponent projectName="ledDisplay"></ProjectComponent>
+            </Route>
+            <Route path="/facial-recognition">
+              <ProjectComponent projectName="facialRecognition"></ProjectComponent>
+            </Route>
+            <Route path="/fatigue-detection">
+              <ProjectComponent projectName="fatigueDetection"></ProjectComponent>
+            </Route>
+            <Route path="/audio-effects">
+              <ProjectComponent projectName="audioEffects"></ProjectComponent>
+            </Route>
+            <Route path="/contact">
+              <ContactComponent></ContactComponent>
+            </Route>
+          </Switch>
+      </Router>
+    </BrowserView>
+    <MobileView>
+      <div className="Mobile">
+        <p> Sorry, this website is not yet supported on mobile</p>
+      </div>
+    </MobileView>
+  </div>
   );
 }
 
