be21da9da3054fb82ee415e3d5b862d3220f7928
Grayson Halliday <graysonhalliday@Graysons-MacBook-Pro-6.local>
2021-08-06 10:48:07 -0700

    Commit Includes: - Fixes build issue with alt prop

--- src/Components/ProjectAssetComponents/TeamSolveAssets.js
+++ src/Components/ProjectAssetComponents/TeamSolveAssets.js
@@ -27,7 +27,7 @@ class TeamSolveAssets extends React.Component {
                         <img style={{display: "block", margin: "auto", height: "500px" }} src="./ProjectAssets/TeamSolve/SolutionPage.jpeg" alt="solution page"></img>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img style={{display: "block", margin: "auto", height: "500px" }} src="./ProjectAssets/TeamSolve/FullScreenImage.jpeg" alt="image"></img>
+                        <img style={{display: "block", margin: "auto", height: "500px" }} src="./ProjectAssets/TeamSolve/FullScreenImage.jpeg" alt="image page"></img>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img style={{display: "block", margin: "auto", height: "500px" }} src="./ProjectAssets/TeamSolve/Comments.jpeg" alt="comments page"></img>
