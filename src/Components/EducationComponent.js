3c3d1e498ed9839b727acaa9ff6b3a7f69b2e3f7
Grayson Halliday <graysonhalliday@Graysons-MacBook-Pro-6.local>
2021-10-28 21:11:17 -0700

    Updated transcript link title

--- src/Components/EducationComponent.js
+++ src/Components/EducationComponent.js
@@ -7,8 +7,8 @@ class EducationComponent extends React.Component {
         <img src="./UVIC_logo.png" alt="UVic logo"/>
         <p class="EducationHeader"> University Of Victoria </p>
         <hr></hr>
-        <p class="EducationHeader"> Electrical Engineering | Graduating August 2021 </p>
-        <a href="./currentTranscript.pdf" style={{marginLeft: "25px"}}>Current Transcript</a>
+        <p class="EducationHeader"> Electrical Engineering | Graduated August 2021 </p>
+        <a href="./currentTranscript.pdf" style={{marginLeft: "25px", fontSize: "22px"}}>Transcript</a>
       </div>
     );
   }
